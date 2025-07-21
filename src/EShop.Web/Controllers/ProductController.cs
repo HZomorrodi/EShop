b66f983beea849bac2067d0b31d5f74dafaf77bc
LOQ <LOQ@DESKTOP-35LRN4K>
2025-07-21 08:05:39 +0330

    Product Details Added.

---
+++ src/EShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using EShop.Services.Contracts;
+using EShop.ViewModels.Products;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EShop.Web.Controllers
+{
+    public class ProductController(IProductService productService) : Controller
+    {
+        private readonly IProductService _productService = productService;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id, string title)
+        {
+            if (id < 1)
+                return View("NotFound");
+            ProductDetailsViewModel? productDetails = await _productService.GetProductDetails(id);
+            if (productDetails is null)
+                return View("NotFound");
+            return View(productDetails);
+        }
+    }
+}
