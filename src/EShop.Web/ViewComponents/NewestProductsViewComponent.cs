056b26103c041e264601b4efe40947a9d2baad7e
LOQ <LOQ@DESKTOP-35LRN4K>
2025-07-23 08:09:29 +0330

    ViewComponent added

---
+++ src/EShop.Web/ViewComponents/NewestProductsViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using EShop.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EShop.Web.ViewComponents
+{
+    public class NewestProductsViewComponent(IProductService productService) : ViewComponent
+    {
+        private readonly IProductService _productService = productService;
+
+        public async Task<IViewComponentResult> InvokeAsync(int? excludeId)
+        {
+            List<ViewModels.Products.ProductPreviewViewModel> model = await _productService.GetNewestProductAsync(excludeId);
+            return View(model);
+        }
+    }
+}
