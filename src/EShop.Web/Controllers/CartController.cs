e89b6cc533f43df49cf23561af6365879ab86aff
268 <268@gmail.com>
2025-07-29 21:43:27 +0330

    Add to Cart Button Continued3

--- src/EShop.Web/Controllers/CartController.cs
+++ src/EShop.Web/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using EShop.DataLayer.Context;
 using EShop.Entities;
 using EShop.Services.Contracts;
 using EShop.Services.EFServices;
+using EShop.ViewModels.Cart;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -32,9 +33,9 @@ namespace EShop.Web.Controllers
             Cart? userCart = await _cartService.GetUserCartAsync(userId);
             if (userCart is null)
             {
-                userCart = new() 
-                { 
-                    UserId = userId 
+                userCart = new()
+                {
+                    UserId = userId
                 };
                 await _cartService.AddAsync(userCart);
             }
@@ -56,6 +57,37 @@ namespace EShop.Web.Controllers
             userCart.TotalPrice += product.Price;
             await _uow.SaveChangesAsync();
             return Json(userCart.TotalPrice.ToString("#,0"));
+        }
+
+        public async Task<string> GetUserCartTotalPrice()
+        {
+            int userId = User.Identity.GetUserId();
+            return (await _cartDetailService.CalculateUserCartTotalPriceAsync(userId)).ToString("#,0");
+        }
+
+        public async Task<PartialViewResult> ShowCartDetailsPreview()
+        {
+            int userId = User.Identity.GetUserId();
+            List<CartDetailPreviewViewModel> cartDetails = await _cartDetailService.GetCartDetailsByAsync(userId);
+            string userCartTotalPrice = cartDetails.Sum(c => c.Price * c.Count).ToString("#,0"); ;
+            ShowCartDetailsViewModel model = new()
+            {
+                CartDetails = cartDetails,
+                UserCartTotalPrice = userCartTotalPrice,
+            };
+            return PartialView("_CartDetailsPartial", model);
+        }
+        public async Task<IActionResult> MyCarts()
+        {
+            int userId = User.Identity.GetUserId();
+            List<ShowCartPreviewForClientViewModel> model = await _cartService.GetUserCartsForClient(userId);
+            return View(model);
+        }
+        public async Task<IActionResult> ShowCartDetails(int id)
+        {
+            int userId = User.Identity.GetUserId();
+            List<CartDetailPreviewViewModel> cartDetails = await _cartDetailService.GetCartDetailsAsync(userId, id);
+            return View(cartDetails);
         }
     }
 }
